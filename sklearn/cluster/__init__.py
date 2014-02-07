93fc3aeb0375d91001652998b12163ae5bcbd537
Jeremy Metz <metz.jp@gmail.com>
2014-02-07 14:41:57 +0000

    Added Fredrik Appelros optics

--- sklearn/cluster/__init__.py
+++ sklearn/cluster/__init__.py
@@ -11,6 +11,7 @@ from .hierarchical import ward_tree, Ward, WardAgglomeration
 from .k_means_ import k_means, KMeans, MiniBatchKMeans
 from .dbscan_ import dbscan, DBSCAN
 from .bicluster import SpectralBiclustering, SpectralCoclustering
+from .optics_ import optics, OPTICS
 
 
 __all__ = ['AffinityPropagation',
@@ -18,6 +19,7 @@ __all__ = ['AffinityPropagation',
            'KMeans',
            'MeanShift',
            'MiniBatchKMeans',
+           'OPTICS',
            'SpectralClustering',
            'Ward',
            'WardAgglomeration',
@@ -27,6 +29,7 @@ __all__ = ['AffinityPropagation',
            'get_bin_seeds',
            'k_means',
            'mean_shift',
+           'optics',
            'spectral_clustering',
            'ward_tree',
            'SpectralBiclustering',
